e909926351a83eab889f3bce6b8a87fe9b980ade
Pedro Sampaio <sampaio_pedro@outlook.com>
2025-11-01 22:42:25 -0300

    Ajustando o deploy no run application

--- infra/backend.tf
+++ infra/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "meu-projeto-aula-terraform-state" # <-- SUBSTITUA AQUI
+    bucket         = "meu-projeto-aula-terraform-state"
     key            = "aula-java/terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
